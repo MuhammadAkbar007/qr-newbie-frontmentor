6af83358405bcc061563223d43813662f2cb9195
Akbar <fotismenos007@mail.ru>
2022-12-28 09:47:01 +0500

    Bismillah

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import qrimage from './assets/image-qr-code.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="text-center wrapper mt-5">
+            <img src={qrimage} alt="qr-image" />
+            <h3 className='title'>Improve your front-end skills by building projects</h3>
+            <p className="text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
   );
 }
 
